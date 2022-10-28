a6dc39ee5deefd72fbeea70b58de84e1b4839f33
robertdenism <robert1994denis@gmail.com>
2022-10-28 18:46:11 +0200

    calculadora

--- app/src/main/java/com/example/proyectoseclipse/calculadora/Calculadora.java
+++ app/src/main/java/com/example/proyectoseclipse/calculadora/Calculadora.java
@@ -1,14 +1,16 @@
-package com.example.proyectoseclipse;
+package com.example.proyectoseclipse.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.proyectoseclipse.R;
+
+public class Calculadora extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculadora);
     }
 }
