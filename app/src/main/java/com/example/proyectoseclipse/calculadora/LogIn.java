f894a50ebe7950e70f84a2c751c73a1906523477
robertdenism <robert1994denis@gmail.com>
2022-11-14 22:35:01 +0100

    prueba remoto

--- app/src/main/java/com/example/proyectoseclipse/calculadora/LogIn.java
+++ app/src/main/java/com/example/proyectoseclipse/calculadora/LogIn.java
@@ -12,5 +12,6 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        //hola
     }
 }
